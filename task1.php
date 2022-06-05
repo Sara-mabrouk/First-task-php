049b7ea2424a4d890e9a16c7303db6481531ed27
Sara Mabrouk <99080325+Sara-mabrouk@users.noreply.github.com>
2022-06-05 21:12:51 +0200

    Initial commit

---
+++ task1.php
@@ -0,0 +1,35 @@
+<?php 
+
+
+// Swap two numbers without using a temporary variable.
+// output : $x=15 , $s=5;
+
+            $x=5;
+            $s=15;
+
+            $x=$x+$s;
+            $s=$x-$s;
+            $x=$x-$s;
+
+echo"value of x: $x </br>";
+// echo "</br>";
+echo"value of s: $s";
+echo "</br>";
+// "</br>";
+// "</br>";
+// "</br>";
+// "</br>";
+// "</br>";
+// "</br>";
+
+// task 2   
+//Write a program to print 2 php variables using single echo statement.
+
+
+$user_1="Hello";
+echo "</br>";
+$user_2=" this is my first project";
+
+echo $user_1.$user_2;
+
+?>
